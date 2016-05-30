c7549212893e7fb47070a9db149fa0ab2b6dd9e8
Prempal Singh <prempal.42@gmail.com>
2016-05-30 23:44:07 +0530

    files recyclerview

--- app/src/main/java/com/prempal/crypt/LoginActivity.java
+++ app/src/main/java/com/prempal/crypt/LoginActivity.java
@@ -1,20 +1,36 @@
 package com.prempal.crypt;
 
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
     TextView appName;
+    EditText username, password;
+    Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
         appName = (TextView) findViewById(R.id.tv_app_name);
+        login = (Button) findViewById(R.id.btn_login);
+
         Typeface face = Typeface.createFromAsset(getAssets(), "lobster-two.bold-italic.ttf");
         appName.setTypeface(face);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this, MainActivity.class));
+            }
+        });
     }
 }
